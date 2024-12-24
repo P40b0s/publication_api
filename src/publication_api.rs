968396ef27c319fe8980f7a06cbe906ab1fe7bd8
noute_xar <uranius666@gmail.com>
2024-12-24 12:35:17 +0300

    add channel for get pages process

--- src/publication_api.rs
+++ src/publication_api.rs
@@ -77,7 +77,7 @@ impl PublicationApi
     }
    
     ///Получение списка документов за определенную дату
-    pub async fn get_documents(date_from: &Date, date_to: Option<&Date>, doc_types: &[String], signatory_authority: Option<&String>, page_number: Option<u32>) -> Result<Vec<PublicationDocumentCard>, PublicationApiError>
+    pub async fn get_documents(date_from: &Date, date_to: Option<&Date>, doc_types: &[String], signatory_authority: Option<&String>, page_number: Option<u32>, sender: Option<tokio::sync::mpsc::Sender<u32>>) -> Result<Vec<PublicationDocumentCard>, PublicationApiError>
     {
         let mut client = Self::client();
         client = client.add_path("Documents");
@@ -87,6 +87,11 @@ impl PublicationApi
         let resp: SearchResult = serde_json::from_slice(&body)?;
         let mut result_vec = Vec::with_capacity(resp.items_total_count as usize);
         result_vec.extend(resp.items);
+        let percentage_mul = 100 / resp.pages_total_count;
+        if let Some(c) = sender.as_ref()
+        {
+            let _ = c.send(percentage_mul * 1).await;
+        }
         for page in (resp.current_page+1)..=resp.pages_total_count
         {
             let params = Self::get_params(date_from, date_to, doc_types, signatory_authority, Some(page));
@@ -94,19 +99,23 @@ impl PublicationApi
             let body = Self::code_error_check(value)?;
             let resp: SearchResult = serde_json::from_slice(&body)?;
             result_vec.extend(resp.items);
+            if let Some(c) = sender.as_ref()
+            {
+                let _ = c.send(percentage_mul * page).await;
+            }
         }
         Ok(result_vec)
     }
     ///получение списка документов с определенной даты
-    pub async fn get_documents_from_date(date_from: &Date, doc_types: &[String], signatory_authority: Option<&String>, page_number: Option<u32>) -> Result<Vec<PublicationDocumentCard>, PublicationApiError>
+    pub async fn get_documents_from_date(date_from: &Date, doc_types: &[String], signatory_authority: Option<&String>, page_number: Option<u32>, channel: Option<tokio::sync::mpsc::Sender<u32>>) -> Result<Vec<PublicationDocumentCard>, PublicationApiError>
     {
-        let resp = Self::get_documents(date_from, None, doc_types, signatory_authority, page_number).await?;
+        let resp = Self::get_documents(date_from, None, doc_types, signatory_authority, page_number, channel).await?;
         Ok(resp)
     }
     ///получение списка документов за период
-    pub async fn get_documents_for_period(date_from: &Date, date_to: &Date, doc_types: &[String], signatory_authority: Option<&String>, page_number: Option<u32>) -> Result<Vec<PublicationDocumentCard>, PublicationApiError>
+    pub async fn get_documents_for_period(date_from: &Date, date_to: &Date, doc_types: &[String], signatory_authority: Option<&String>, page_number: Option<u32>, channel: Option<tokio::sync::mpsc::Sender<u32>>) -> Result<Vec<PublicationDocumentCard>, PublicationApiError>
     {
-        let resp = Self::get_documents(date_from, Some(date_to), doc_types, signatory_authority, page_number).await?;
+        let resp = Self::get_documents(date_from, Some(date_to), doc_types, signatory_authority, page_number, channel).await?;
         Ok(resp)
     }
     /// список видов документов  
@@ -162,8 +171,9 @@ impl PublicationApi
     }
 
      /// получение первого документа (для оценки что у него за номер итд....)
+     /// если документов не найдено возвращает None
     ///http://95.173.157.131/api/Documents?SignatoryAuthorityId=8d31525e-fafc-4590-8580-422f588d20c9&DocumentTypes=2dddb344-d3e2-4785-a899-7aa12bd47b6f&pageSize=10&index=1
-    pub async fn get_first_document(sa: &str, doc_type: &str) -> Result<PublicationDocumentCard, PublicationApiError>
+    pub async fn get_first_document(sa: &str, doc_type: &str) -> Result<Option<PublicationDocumentCard>, PublicationApiError>
     {
         let mut client = Self::client();
         client = client.add_path("Documents");
@@ -176,14 +186,8 @@ impl PublicationApi
         let value = client.get_with_params(&params).await?;
         let body = Self::code_error_check(value)?;
         let resp: SearchResult = serde_json::from_slice(&body)?;
-        if let Some(first_item) = resp.items.into_iter().next()
-        {
-            Ok(first_item)
-        }
-        else 
-        {
-            Err(PublicationApiError::ApiError("Не найдено ни одного документа".to_owned()))
-        }
+        let first_item =  resp.items.into_iter().next();
+        Ok(first_item)
     }
 
 
@@ -260,7 +264,16 @@ mod tests
     async fn test_get_images()
     {
         StructLogger::new_default();
-        let u = PublicationApi::get_documents_from_date(&Date::parse("01.04.2024").unwrap(), &["82a8bf1c-3bc7-47ed-827f-7affd43a7f27".to_owned()],None, None).await.unwrap();
+        let (sender, mut receiver) =  tokio::sync::mpsc::channel::<u32>(7);
+        let handle = tokio::spawn(
+            async move 
+            {
+                while let Some(p) = receiver.recv().await 
+                {
+                    println!("текущий процент выполнения: {}%", p);
+                }
+            }).await;
+        let u = PublicationApi::get_documents_from_date(&Date::parse("01.04.2024").unwrap(), &["82a8bf1c-3bc7-47ed-827f-7affd43a7f27".to_owned()],None, None, Some(sender)).await.unwrap();
         //let mut d = PublicationDocumentCard { eo_number: "0001202406220019".to_owned(), has_svg: false, zip_file_length: None, publish_date_short:  Date::parse("2024-06-22T00:00:00").unwrap(), complex_name: "Федеральный закон от 22.06.2024 № 160-ФЗ\n \"О внесении изменений в статью 19 Федерального закона \"О крестьянском (фермерском) хозяйстве\" и Федеральный закон \"О развитии сельского хозяйства\"".to_owned(), pages_count: 4, curr_page: 0, pdf_file_length: 169841, jd_reg_number: None, jd_reg_date: None, title: "Федеральный закон от 22.06.2024 № 160-ФЗ<br /> \"О внесении изменений в статью 19 Федерального закона \"О крестьянском (фермерском) хозяйстве\" и Федеральный закон \"О развитии сельского хозяйства\"".to_owned(), view_date: Date::parse("2024-06-22T00:00:00").unwrap(), id: "118e71c6-7e90-495c-9afb-56b38edea17a".to_owned() };
         let mut d: PublicationDocumentCard = u[0].clone();
         logger::info!("{:?}", &d);
@@ -320,12 +333,24 @@ mod tests
     async fn test_one_year_request()
     {
         StructLogger::new_default();
+        let (sender, mut receiver) =  tokio::sync::mpsc::channel::<u32>(1);
+        let h = tokio::spawn(
+            async move 
+            {
+                while let Some(p) = receiver.recv().await 
+                {
+                    logger::info!("текущий процент выполнения: {}%", p);
+                }
+            });
         let u = PublicationApi::get_documents_for_period(
             &Date::parse("01.01.2024").unwrap(),
             &Date::parse("13.12.2024").unwrap(),
             &["0790e34b-784b-4372-884e-3282622a24bd".to_owned()],
-            Some("225698f1-cfbc-4e42-9caa-32f9f7403211".to_owned()).as_ref(), None
+            Some("225698f1-cfbc-4e42-9caa-32f9f7403211".to_owned()).as_ref(),
+            None,
+            Some(sender)
         ).await.unwrap();
+        let _ = h.await;
         logger::debug!("Найдено {} документов", u.len());
     }
     #[tokio::test]
